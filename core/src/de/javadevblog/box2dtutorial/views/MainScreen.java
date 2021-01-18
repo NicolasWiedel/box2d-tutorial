7753928ce25489f88eca85f2288a5703f6c2d824
nicolaswiedel <nicolaswiedel@googlemail.com>
2021-01-18 16:37:25 +0100

    End Chapter 3

--- core/src/de/javadevblog/box2dtutorial/views/MainScreen.java
+++ core/src/de/javadevblog/box2dtutorial/views/MainScreen.java
@@ -1,14 +1,27 @@
 package de.javadevblog.box2dtutorial.views;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import de.javadevblog.box2dtutorial.B2dModel;
 import de.javadevblog.box2dtutorial.Box2DTutorial;
 
 public class MainScreen implements Screen {
 
     private Box2DTutorial game;
+    private B2dModel model;
+    private OrthographicCamera cam;
+    private Box2DDebugRenderer debugRenderer;
 
     public MainScreen(Box2DTutorial game){
         this.game = game;
+
+        model = new B2dModel();
+        cam = new OrthographicCamera(32, 24);
+        debugRenderer = new Box2DDebugRenderer(true, true, true,
+                true, true,true);
     }
 
     @Override
@@ -18,7 +31,10 @@ public class MainScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
+        model.logicStep(delta);
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        debugRenderer.render(model.getWorld(), cam.combined);
     }
 
     @Override
